4f7249fde5c18e58fc9d815eeaf87b9d4aaf1a3a
stessarini <stefan.tessarini@psi.ch>
2022-01-26 18:02:53 +0100

    add license header to DetectorZPlane.h

--- EGS_XGI/Diffraction/OpticalElement/DetectorObject/DetectorZPlane/DetectorZPlane.h
+++ EGS_XGI/Diffraction/OpticalElement/DetectorObject/DetectorZPlane/DetectorZPlane.h
@@ -1,3 +1,35 @@
+/*
+###############################################################################
+#
+#   EGS_XGI DetectorZPlane header
+#   A basic detector class (orthogonal to z-axis).
+#   Copyright (C) 2020  ETH Zürich
+#
+#   This file is part of the EGS_XGI - an X-ray grating interferometry
+#   extension for EGSnrc.
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published
+#   by the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###############################################################################
+#
+#   Author:     Stefan Tessarini
+#
+#
+#
+###############################################################################
+*/
+
 #ifndef _DETECTORZPLANE_H_
 #define _DETECTORZPLANE_H_
 
@@ -44,18 +76,13 @@ protected:
 	unsigned int m_nNxTotalSignal;
 	unsigned int m_nNyTotalSignal;
 
-	//unsigned int m_nNxHistorySignal;
-	//unsigned int m_nNyHistorySignal;
-
-	EGS_Vector m_fPosition;
+		EGS_Vector m_fPosition;
 	EGS_Float m_fWidth;
 	EGS_Float m_fHeight;
 
 	EGS_Float m_f_TotalSignal_DeltaX; //Bin width
 	EGS_Float m_f_Totalsignal_DeltaY;
 
-	//EGS_Float m_f_HistorySignal_DeltaX;
-	//EGS_Float m_f_HistorySignal_DeltaY;
 
 	bool m_bWirteToBinary;
 	bool m_bWriteToPGM;
