394d13d7115c6c471681b8c252a3ba55bc2d6a40
stessarini <stefan.tessarini@psi.ch>
2022-01-26 20:01:12 +0100

    header BinarySourceGratingFourierZPlane

--- EGS_XGI/Diffraction/OpticalElement/SplittingObject/BinarySourceGratingFourierZPlane/BinarySourceGratingFourierZPlane.cpp
+++ EGS_XGI/Diffraction/OpticalElement/SplittingObject/BinarySourceGratingFourierZPlane/BinarySourceGratingFourierZPlane.cpp
@@ -1,3 +1,34 @@
+/*
+###############################################################################
+#
+#   EGS_XGI BinarySourceGratingFourierZPlane
+#	  Binary source grating orthogonal to the z-axis.
+#   Copyright (C) 2020  ETH Zürich
+#
+#   This file is part of the EGS_XGI - an X-ray grating interferometry
+#   extension for EGSnrc.
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published
+#   by the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###############################################################################
+#
+#   Author:     Stefan Tessarini
+#
+#
+#
+###############################################################################
+*/
 #include "BinarySourceGratingFourierZPlane.h"
 
 #include "xgi_global_variables.h"
--- EGS_XGI/Diffraction/OpticalElement/SplittingObject/BinarySourceGratingFourierZPlane/BinarySourceGratingFourierZPlane.h
+++ EGS_XGI/Diffraction/OpticalElement/SplittingObject/BinarySourceGratingFourierZPlane/BinarySourceGratingFourierZPlane.h
@@ -1,3 +1,34 @@
+/*
+###############################################################################
+#
+#   EGS_XGI BinarySourceGratingFourierZPlane header
+#	  Binary source grating orthogonal to the z-axis.
+#   Copyright (C) 2020  ETH Zürich
+#
+#   This file is part of the EGS_XGI - an X-ray grating interferometry
+#   extension for EGSnrc.
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published
+#   by the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###############################################################################
+#
+#   Author:     Stefan Tessarini
+#
+#
+#
+###############################################################################
+*/
 #ifndef _BINARYSOURCEGRATINGFOURIERZPLANE_H_
 #define _BINARYSOURCEGRATINGFOURIERZPLANE_H_
 
@@ -7,7 +38,6 @@
 #include "egs_rndm.h"
 
 #include "SplittingObject.h"
-//#include <vector>
 
 class BinarySourceGratingFourierZPlane : public SplittingObject
 {
